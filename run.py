a7710cee3854c61dd186b4421dfd87f07b2f71b4
phongviet <phongviet2912@gmail.com>
2024-08-29 10:43:24 +0700

    first project

---
+++ run.py
@@ -0,0 +1,92 @@
+# %% Initialize the drawing window
+import cv2
+import numpy as np
+
+image = np.array([0]*28*28*3*121, dtype=np.uint8).reshape(28*11,28*11,3)
+
+# %% Mouse callback function
+drawing = False # true if mouse is pressed
+ 
+def draw_circle(event,x,y,flags,param):
+    global drawing, image
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+ 
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.rectangle(image,(x-5,y-5),(x+5,y+5),(255,255,255),-1)
+ 
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(image,(x-5,y-5),(x+5,y+5),(255,255,255),-1)
+    
+    elif event == cv2.EVENT_RBUTTONDOWN: #Reset image
+        image = np.array([0]*28*28*3*121, dtype=np.uint8).reshape(28*11,28*11,3)
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_circle)
+
+# %% Prepare trained model
+import torch
+from torch import nn
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3, 1) # 28x28x1 -> 26x26x16
+        #pool -> 13x13x16
+        self.conv2 = nn.Conv2d(16, 32, 3, 1) # 13x13x16 -> 11x11x32
+        #pool -> 5x5x32
+        self.flatten = nn.Flatten()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(5*5*32, 800)
+        self.fc2 = nn.Linear(800, 800)
+        self.fc3 = nn.Linear(800, 10)
+        self.relu = nn.ReLU()
+    def forward(self, X):
+        X = self.conv1(X)
+        X = self.relu(X)
+        X = self.pool(X)
+        X = self.conv2(X)
+        X = self.relu(X)
+        X = self.pool(X)
+        X = self.flatten(X)
+        X = self.relu(self.fc1(X))
+        X = self.relu(self.fc2(X))
+        X = self.fc3(X)
+        return X
+    
+model = NeuralNetwork()
+model.load_state_dict(torch.load('model.pth'))
+model.eval()
+
+# %% Transform image to feed to model
+def transform(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.resize(image, (28, 28))
+    image = torch.tensor(image, dtype=torch.float32)
+    return image
+
+# %% Feed image to model
+def feed(image):
+    image = image.unsqueeze(0).unsqueeze(0).float()
+    return model(image)
+
+# %% Run loop
+i = 0
+while True:
+    cv2.imshow('image', image)
+    guess_img = image.copy()
+    guess_img = transform(guess_img)
+    with torch.no_grad():
+        guess = np.argmax(feed(guess_img))
+        if i % 100 == 0: print(int(guess))
+        #print guess on the image with delay
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    i += 1
+
+cv2.destroyAllWindows()
+# %%
+#test = cv2.imread('test.png')
+# %%
---
+++ train.py
@@ -0,0 +1,140 @@
+# %% Prepare data
+import torchvision.datasets as datasets
+import numpy as np
+
+mnist_trainset = datasets.MNIST(root='./data', train=True, 
+                                download=True, transform=None)
+mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
+train_set = mnist_trainset.data.numpy()
+test_set = mnist_testset.data.numpy()
+train_labels = mnist_trainset.targets.numpy()
+test_labels = mnist_testset.targets.numpy()
+
+# %% Import libraries
+from sklearn.model_selection import train_test_split
+import torch.nn as nn
+import torch
+from torch.utils.data import DataLoader, Dataset 
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+# %% Show an image
+def imshow(img):
+    plt.imshow(img, cmap='gray')
+    plt.show()
+
+# %% Show random 9 images
+import random
+random_range = random.sample(range(0, len(train_set)), 9)
+j = 0
+for i in random_range:
+    plt.subplot(3,3,j+1)
+    plt.imshow(train_set[i],cmap='gray')
+    plt.title(train_labels[i])
+    j += 1
+
+# %% Dataset class
+class MNISTDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = torch.tensor(X, dtype=torch.float32).to('cuda')
+        self.y = torch.tensor(y, dtype=torch.int64).to('cuda')
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+    
+# %% split train set and dev set
+X_train, X_dev, y_train, y_dev = train_test_split(train_set, train_labels, test_size=0.2, random_state=0)
+
+#%% #train on training set only  
+#train_loader = DataLoader(MNISTDataset(X_train, y_train), batch_size=64, shuffle=True)
+#train on whole set
+train_loader = DataLoader(MNISTDataset(train_set, train_labels)
+                          , batch_size=64, shuffle=False,)
+test_loader = DataLoader(MNISTDataset(X_dev, y_dev)
+                         , batch_size=64, shuffle=False)
+# %% Network class
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3, 1) # 28x28x1 -> 26x26x16
+        #pool -> 13x13x16
+        self.conv2 = nn.Conv2d(16, 32, 3, 1) # 13x13x16 -> 11x11x32
+        #pool -> 5x5x32
+        self.flatten = nn.Flatten()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(5*5*32, 800)
+        self.fc2 = nn.Linear(800, 800)
+        self.fc3 = nn.Linear(800, 10)
+        self.relu = nn.ReLU()
+    def forward(self, X):
+        X = self.conv1(X)
+        X = self.relu(X)
+        X = self.pool(X)
+        X = self.conv2(X)
+        X = self.relu(X)
+        X = self.pool(X)
+        X = self.flatten(X)
+        X = self.relu(self.fc1(X))
+        X = self.relu(self.fc2(X))
+        X = self.fc3(X)
+        return X
+#%% Load model
+model = NeuralNetwork()
+model.to('cuda')
+#model.load_state_dict(torch.load('model.pth'))
+#model.eval()
+
+#%% Train the model
+# Hyperparameters
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+losses = []
+
+#%% 1000 epochs with batch_size=64
+for epoch in range(1000):
+    for i, (X_batch, y_batch) in enumerate(train_loader):
+        optimizer.zero_grad()
+        y_pred = model(X_batch.unsqueeze(1).float())
+        loss = loss_fn(y_pred, y_batch)
+        loss.backward()
+        optimizer.step()
+    if epoch % 100 == 0:
+        print(f'Epoch {epoch}, Loss: {loss.item()}')
+        losses.append(loss.item())
+
+# %% Save the model
+torch.save(model.state_dict(), 'model.pth')
+
+# %% Accuracy evaluation on dev set
+from sklearn.metrics import accuracy_score
+with torch.no_grad():
+    y_pred = model(torch.tensor(X_dev, dtype=torch.float32).unsqueeze(1).to('cuda')).argmax(dim=1).cpu().numpy()
+    print(f'Accuracy: {accuracy_score(y_dev, y_pred)}')
+
+# %% Accuracy evaluation on test set
+with torch.no_grad():
+    y_pred = model(torch.tensor(test_set, dtype=torch.float32).unsqueeze(1).to('cuda')).argmax(dim=1).cpu().numpy()
+    print(f'Accuracy: {accuracy_score(test_labels, y_pred)}')
+
+# %% Plot the losses
+x = [i*100 for i in range(len(losses))]
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.plot(x, losses)
+# %% Guess one image
+import cv2
+import numpy as np
+image = cv2.imread('test.png')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.resize(image, (28, 28))
+image = np.array(image, dtype=np.int64)
+image = torch.tensor(image, dtype=torch.float32)
+image = image.unsqueeze(0).unsqueeze(0).float()
+with torch.no_grad():
+    guess = np.argmax(model(image.to('cuda')).cpu().numpy())
+    print(guess)
+#
+# %%
